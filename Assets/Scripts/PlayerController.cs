bfc2ca6b0033f17aa208a4171a873b29c8be917e
Byung charn lee <byung@motion.local>
2018-10-24 16:15:59 +0530

    Player movement and jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public bool grounded;
+    public float jumpForce;
+
+    private Collider PlayerCollider;
+    private Rigidbody PlayerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        PlayerCollider = GetComponent<Collider>();
+        PlayerRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 newPos = new Vector3(PlayerRigidBody.transform.position.x, PlayerRigidBody.transform.position.y, PlayerRigidBody.transform.position.z + 2.0f);
+            PlayerRigidBody.transform.position = newPos;
+        }else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 newPos = new Vector3(PlayerRigidBody.transform.position.x, PlayerRigidBody.transform.position.y, PlayerRigidBody.transform.position.z - 2.0f);
+            PlayerRigidBody.transform.position = newPos;
+        }else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerRigidBody.velocity = Vector3.up * jumpForce;
+        }
+	}
+}
